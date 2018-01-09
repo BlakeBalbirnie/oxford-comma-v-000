bac0705a492e4dd9b34ad80c935c3f4ca3d28c12
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-09 15:29:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   last_item = array.pop
   if array.count == 0
     return last_item
-  elsif array_count == 1
+  elsif array.count == 1
     return "#{array[0]} and #{last_item}"
   else
     return "#{array.join(", ")}, and #{last_item}"
